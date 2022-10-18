0b8891ca6df88997b23c96cc4cc6c12eeddc7716
Aden Garbutt <aden@Adens-MacBook-Pro.local>
2022-10-18 08:27:47 -0400

    upload

---
+++ 12_flask-forms/app.py
@@ -0,0 +1,66 @@
+'''
+JANitors: Jing Feng, Nicole Zhou, Aden Garbutt
+SoftDev
+K12 -- Take and Give
+2022-10-17
+time spent: 1.3 hr
+'''
+
+#the conventional way:
+from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+
+'''
+trioTASK:
+~~~~~~~~~~~ BEFORE RUNNING THIS, ~~~~~~~~~~~~~~~~~~
+...read for understanding all of the code below.
+Some will work as written; other sections will not. 
+TASK: Predict which...
+Devise some simple tests you can run to "take apart this engine," as it were.
+Execute your tests.
+Process results.
+
+PROTIP: Insert your own in-line comments
+ wherever they will help
+  your future self and/or current teammates
+   understand what is going on.
+'''
+
+@app.route("/", methods=['GET', 'POST'])
+def disp_loginpage():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.form)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    return render_template( 'login.html' )
+
+
+@app.route("/auth" , methods=['GET', 'POST'])
+def authenticate():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.form)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    return render_template ( "response.html", response = request.form.get('username'), method = request.method) #response to a form submission
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
---
+++ 12_flask-forms/readme.md
@@ -0,0 +1,14 @@
+*JANitors: Jing Feng, Nicole Zhou, Aden Garbutt*\
+*SoftDev*\
+*K12 -- Take and Give*\
+*2022-10-17*\
+*time spent: 1.3 hr*
+
+### DISCOS:
+- response = request.form.get('username') in app.py and {{username}} in response.html to get inputted username
+- method = request.method and {{method}} in response.html to get method used
+- GET displays all the info in the webpage in the URL
+- query strings (POST method)+ request.args.get() prevents app from failing
+
+### QCC:
+- When is it better to use GET than POST?
