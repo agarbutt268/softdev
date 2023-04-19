507f107483405fe6d3ec386c4e77097df435e397
Aden Garbutt <agarbutt268@gmail.com>
2023-04-19 13:03:10 -0400

    dom js tingz

---
+++ 28_jsdom/dommy_consolery.js
@@ -0,0 +1,132 @@
+// Team GASLIGHTING GATEKEEPING GIRLBOSSES :: Kevin Xiao + Aden Garbutt
+// SoftDev pd7
+// K28 -- Getting more comfortable with the dev console and the DOM
+// 2023-04-05w
+// --------------------------------------------------
+
+
+//send diagnostic output to console
+//(Ctrl-Shift-K in Firefox to reveal console)
+console.log("AYO");
+
+var i = "hello";
+var j = 20;
+
+
+//assign an anonymous fxn to a var
+var f = function(x) {
+  var j=30;
+  return j+x;
+};
+
+
+//instantiate an object
+var o = { 'name' : 'Thluffy',
+          age : 1024,
+          items : [10, 20, 30, 40],
+          morestuff : {a : 1, b : 'ayo'},
+          func : function(x) {
+            return x+30;
+          }
+        };
+
+
+var addItem = function(text) {
+  var list = document.getElementById("thelist");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = text;
+  list.appendChild(newitem);
+};
+
+
+var removeItem = function(n) {
+  var listitems = document.getElementsByTagName('li');
+  listitems[n].remove();
+};
+
+
+var red = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    items[i].classList.add('red');
+  }
+};
+
+
+var stripe = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    if (i%2==0){
+      items[i].classList.add('red');
+    } else {
+      items[i].classList.add('blue');
+    }
+  }
+};
+
+//insert your implementations here for...
+// FIB
+
+function fib(n) {
+  if (n < 2){
+      return 1
+  } else{
+      return fib(n - 1) + fib(n - 2)
+  }
+}
+
+console.log(fib(1))
+console.log(fib(2))
+console.log(fib(3))
+console.log(fib(4))
+console.log(fib(5))
+/*
+var fib = function(text) {
+  var list = document.getElementById("theli");
+  var newitem = document.createElement("li");
+  newitem.innerHTML = text;
+  list.appendChild(newitem);
+};
+*/
+
+
+addItem("This is fibonnaci numero uno: " + fib(1));
+
+
+// FAC
+
+function fact(n) {
+  if (n < 2){
+      return 1
+  } else{
+      return n * fact(n - 1)
+  }
+}
+
+console.log(fact(1))
+console.log(fact(2))
+console.log(fact(3))
+console.log(fact(4))
+console.log(fact(5))
+
+addItem("This is factorial numero quadro: " + fact(4));
+
+// GCD
+
+var gcd = function(a, b) {
+  if (!b) {
+    return a;
+  }
+
+  return gcd(b, a % b);
+}
+
+addItem("This is the greatest common denominator or 12 and 8: " + gcd(12, 8))
+
+// In addition to the style shown above,
+//  you are encouraged to test drive the "arrow function syntax" as shown below.
+//  Note anything notable.
+const myFxn = (param1, param2) => {
+  // body
+  return retVal;
+};
