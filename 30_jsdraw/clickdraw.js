fa4dc443d33fe400197d69e1814525babfe0f3a6
Aden Garbutt <agarbutt268@gmail.com>
2023-04-24 15:14:58 -0400

    ms paint beta loading

---
+++ 30_jsdraw/clickdraw.js
@@ -0,0 +1,56 @@
+// Team GASLIGHTING GATEKEEPING GIRLBOSSES :: Kevin Xiao + Aden Garbutt
+// SoftDev pd7
+// K30 -- canvas based JS drawing
+// 2023-04-24m
+
+// retrieve node in DOM via ID
+var c = document.getElementById("slate");
+var buttonToggle = document.getElementById("buttonToggle")
+var buttonClear = document.getElementById("buttonClear")
+
+//instantiatWe a CanvasRenderingContext2D object
+var ctx = c.getContext("2d");
+
+var mode = "rect";
+
+//var toggleMode = function(e) {
+var toggleMode = (e) => {
+    //console.log("toggling...");
+    if (mode == "rect") {
+      mode = "circle";
+      buttonToggle.innerHTML = "circle";
+    }
+    else {
+      mode = "rect";
+      buttonToggle.innerHTML = "rect";
+    }
+}
+
+var drawRect = function(e) {
+  var mouseX = MouseEvent.clientX;
+  var mouseY = MouseEvent.clientY;
+  console.log("mouseclick registered at ", mouseX, mouseY);
+}
+
+var drawCircle = (e) => {
+  var mouseX = `${e.clientX}`
+  var mouseY = `${e.clientY}`
+  console.log("mouseclick registered at ", mouseX, mouseY);
+}
+
+var draw = (e) => {
+  if (mode == "rect") {
+    drawRect();
+  }
+  else {
+    drawCircle();
+  }
+}
+
+var wipeCanvas = () => {
+
+}
+
+c.addEventListener("click", draw);
+buttonToggle.addEventListener("click", toggleMode);
+buttonClear.addEventListener("click", wipeCanvas);
