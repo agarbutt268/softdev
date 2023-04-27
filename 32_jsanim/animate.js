a19fc9f9a880e52d43fb9b3a14c1aa009ef5c9ce
Aden Garbutt <agarbutt268@gmail.com>
2023-04-26 23:08:18 -0400

    watch till ht corner

--- 32_jsanim/animate.js
+++ 32_jsanim/animate.js
@@ -1,50 +1,86 @@
-//access canvas and buttons via DOM (uodate HTML source to align)
-var c = document.getElementBId("plavaround");
-var dotButton = document.getElementBid("circle");
-var dvdButton = document.getelementById("dvd");
-var stopButton = document.getelementById("stop");
+//access canvas and buttons via DOM (upodate HTML source to align)
+var c = document.getElementById("playground");
+var dotButton = document.getElementById("circle");
+var dvdButton = document.getElementById("dvd");
+var stopButton = document.getElementById("stop");
+
+var ctx = c.getContext("2d");
+var requestID;
 
 //most/all drawdot code omitted. Carry your best almagamated code forward from last work to retain dot grow/shrink functionality.
 var clear = function(e) {
-  e.preventDefault ( ); //Q:whatdis?
+  e.preventDefault(); //Q:whatdis?
   ctx.clearRect (0, 0, 500, 500);
 };
 
-var dvdLogoSetup = function () {
-  window.cancelAnimationFrame ( requestID );
-  var rectwidth = // ???
-  var rectheight = // ???
+var radius = 0;
+var growing = true;
 
-  var rectx = //construct for selecting random valid xcor
-  var rectY = //construct for selecting random valid cor
-  var XVel = // ???
-  var yVel = // ???
+var drawDot = (e) => {
+    window.cancelAnimationFrame(requestID);
+    ctx.clearRect( 0, 0, c.width, c.height );
+
+    ctx.beginPath();
+    ctx.arc(c.width / 2, c.height / 2, radius, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+
+    if (growing == true){
+        radius += 1;
+    }
+    else {
+        radius -= 1;
+    }
+    if (radius > c.width / 2){
+        growing = false;
+    }
+    if (radius == 0){
+        growing = true;
+    }
+    requestID = window.requestAnimationFrame(drawDot);
+}
+
+
+var dvdLogoSetup = function (e) {
+  window.cancelAnimationFrame(requestID);
+  var rectwidth = 60;
+  var rectheight = 40;
+
+  var rectX = Math.floor(Math.random() * (500-rectwidth));
+  var rectY = Math.floor(Math.random() * (500-rectheight));
+  var xVel = 3;
+  var yVel = 2;
   var logo = new Image ();
 
   logo.src = "logo_dvd.jpg";
-  var dvdLogo = function) {
-    ctx.clearRect ( 0, 0, c.width, c.height );
-    //ctx.fillect ( rectx, rectY, rectwidth, rectHeight );
-    ctx. drawImage( logo, rectX, , rectY, rectwidth, rectHeight );
-    if ( /* bounce criteria */ ) {
-      xVel = // ???
+
+  var dvdLogo = function () {
+    ctx.clearRect( 0, 0, c.width, c.height );
+    //ctx.fillRect( rectX, rectY, rectwidth, rectheight );
+    ctx.drawImage(logo, rectX, rectY, rectwidth, rectheight);
+
+    if (rectX+rectwidth >= 500 || rectX < 0) {
+      xVel = -1 * xVel;
     }
 
-    if ( /* bounce criteria */ ) {
-      yVel = // ???
+    if (rectY+rectheight >= 500 || rectY < 0) {
+      yVel = -1 * yVel;
     }
-    rectx = // ???
-    recty = // ???
-    requestID = window.requestAnimationFrame ( /* ??? */ );
+
+    rectX += xVel;
+    rectY += yVel;
+    requestID = window.requestAnimationFrame(dvdLogo);
   };
   dvdLogo ();
 };
+
 
 var stopIt = function() {
   console.log(requestID);
   window.cancelAnimationFrame(requestID);
 };
 
-dotButton.addEventListener("click", drawdot);
-dvdButton.addEventListener("click", stopIt);
-stopButton.addEventListener("click", dvdLogoSetup);
+
+dotButton.addEventListener("click", drawDot);
+dvdButton.addEventListener("click", dvdLogoSetup);
+stopButton.addEventListener("click", stopIt);
